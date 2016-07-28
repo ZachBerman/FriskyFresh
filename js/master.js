d2e672022752dfca769b99351546ad8cd031dcba
Zach Berman <zberms@gmail.com>
2016-07-28 02:45:51 +0000

    Tweak jquery validations and contact form setup

--- js/master.js
+++ js/master.js
@@ -1,9 +1,2 @@
-//
-$(document).ready(function(){
-    $("form").submit(function(){
-        $("input.send").replaceWith("<h4 class=text-center>Message Sent!</h4>");
-    });
-});
-
 // Validate form input fields are not empty(uses jQuery validate plugin)
 $("#commentForm").validate();
